84c28728e75a4aa9d7d43147d92a0c88a844b506
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-09-20 18:35:18 -0700

    Added test Place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,69 +1,101 @@
 #!/usr/bin/python3
-""" """
+"""
+Test Place class
+"""
+
 from tests.test_models.test_base_model import test_basemodel
 from models.place import Place
 
 
 class test_Place(test_basemodel):
-    """ """
+    """
+    Test Place class
+    """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """
+        Init method
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
         super().__init__(*args, **kwargs)
         self.name = "Place"
         self.value = Place
 
-    def test_city_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.city_id), str)
+    def test_city(self):
+        """
+        Test city_id attribute
+        """
+        new_city = self.value()
+        self.assertEqual(type(new_city.city_id), str)
 
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
-
-    def test_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
-
-    def test_description(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.description), str)
-
-    def test_number_rooms(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.number_rooms), int)
-
-    def test_number_bathrooms(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.number_bathrooms), int)
-
-    def test_max_guest(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.max_guest), int)
-
-    def test_price_by_night(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.price_by_night), int)
+    def test_price(self):
+        """
+        Test price_by_night attribute
+        """
+        new_inst = self.value()
+        self.assertEqual(type(new_inst.price_by_night), int)
 
     def test_latitude(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.latitude), float)
+        """
+        Test latitude attribute
+        """
+        new_inst = self.value()
+        self.assertEqual(type(new_inst.latitude), float)
 
     def test_longitude(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.latitude), float)
+        """
+        Test longitude attribute
+        """
+        new_inst = self.value()
+        self.assertEqual(type(new_inst.latitude), float)
 
-    def test_amenity_ids(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.amenity_ids), list)
+    def test_descr(self):
+        """
+        Test description attribute
+        """
+        new_descr = self.value()
+        self.assertEqual(type(new_descr.description), str)
+
+    def test_nums(self):
+        """
+        Test number_rooms attribute
+        """
+        new_num = self.value()
+        self.assertEqual(type(new_num.number_rooms), int)
+
+    def test_n_baths(self):
+        """
+        Test number_bathrooms attribute
+        """
+        new_num_baths = self.value()
+        self.assertEqual(type(new_num_baths.number_bathrooms), int)
+
+    def test_maximum_guests(self):
+        """
+        Test max_guest attribute
+        """
+        new_inst = self.value()
+        self.assertEqual(type(new_inst.max_guest), int)
+
+    def test_amenity(self):
+        """
+        Test amenity_ids attribute
+        """
+        new_inst = self.value()
+        self.assertEqual(type(new_inst.amenity_ids), list)
+
+    def test_user(self):
+        """
+        Test user_id attribute
+        """
+        new_user = self.value()
+        self.assertEqual(type(new_user.user_id), str)
+
+    def test_name(self):
+        """
+        Test name attribute
+    """
+        new_name = self.value()
+        self.assertEqual(type(new_name.name), str)
